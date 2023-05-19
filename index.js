5bf5930017a2dac3744d14835a1d5a0c8fb4b250
Leslie Harden Cooper <lesliehcooper@gmail.com>
2023-05-19 11:19:39 -0500

    All tests passed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(array, string) {
+  return array.filter(function (driverName) {
+    return driverName.toLowerCase() === string.toLowerCase();
+  });
+}
+
+
+findMatching(drivers, "Bobby");
+
+function fuzzyMatch(array, string) {
+  let length = string.length; 
+  return array.filter(function (provLetter) {
+    return provLetter.slice(0, length) === string;
+  });
+}
+
+fuzzyMatch(drivers, "Bo");
+
+function matchName(array, string) {
+  return array.filter(function (matchTheName) {
+    return matchTheName.name.toLowerCase() === string.toLowerCase();
+  });
+}
+
+matchName(drivers, "Bobby");
